d0e7eb67c5b6dbf810df686ff03f581bdcbf61a5
volodinaa87 <32937840+volodinaa87@users.noreply.github.com>
2019-03-01 21:26:47 +0300

    Update index_exw2_1.js

--- index_exw2_1.js
+++ index_exw2_1.js
@@ -2,63 +2,12 @@
  * @param {String} tweet
  * @returns {String[]}
  */
-/*module.exports = function (tweet) {
-    var arrSplit = tweet.split(' ');
-    var arrHash = [];
-    for (var i = 0; i < arrSplit.length; i++) {
-        if (arrSplit[i][0] == '#') {
-            arrHash.push(arrSplit[i].substring(1));
-            //arrHash.push(arrSplit[i]);
-        }
-    }
-        return arrHash;
-    };
-*/
-
-/*module.exports = function (tweet) {
-
-    var arrSplit = tweet.split(' ');
+module.exports = function (tweet) {
     var filteredArr=[];
-    arrSplit.forEach(function(item){
-        if (item[0] == '#') {
+    tweet.split(' ').forEach(function(item){
+        if (item[0] === '#') {
             filteredArr.push(item.replace('#',''));
         }
-      });
-      return filteredArr;
-};
-*/
-module.exports = 
-function (tweet) {
-    var arrSplit = tweet.split(' ')
-                        .filter(filterByHash)
-                        //.forEach(function(element) {element.replace('#','');})
-                        //.map(function(element) {element.replace('#','');})
-                        ;
-    
-    //var filteredArr=[];
-    //arrSplit.filter(filterByHash
-
-    var filteredArr = arrSplit
-    //arrSplit //.filter(filterByHash
-        /*arrSplit.forEach(function(item){
-        
-            if (item[0] == '#') {
-                filteredArr.push(item.replace('#',''));
-            }
-        })*/
-
-      //)
-      ;
-      return filteredArr;
-}
-//var filteredArr=[];
-
-function filterByHash(item) {
-    if (item[0] == '#') {
-        //filteredArr.push(item.replace('#',''));
-        //return item;
-        //item.replace('#','');
-        return true;
-    }
-    return false;
+    });
+    return filteredArr;
 };
