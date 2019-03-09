3c9e8464ecf2832bb73b18055688732b5c4ce1db
volodinaa87 <32937840+volodinaa87@users.noreply.github.com>
2019-03-09 11:14:37 +0300

    Update index_w2_ex2.js

--- index_w2_ex2.js
+++ index_w2_ex2.js
@@ -19,3 +19,30 @@ module.exports = function (hashtags) {
             .toString();
 return lowers;
 };
+//////////////////////
+//решение перебором
+//     nextInput:
+//     for (var i = 0; i < lowers.length; i++) {
+//         var str = lowers[i]; // для каждого элемента
+//         for (var j = 0; j < resultHash.length; j++) { // ищем, был ли он уже?
+//             if (resultHash[j] == str) continue nextInput; // если да, то следующий
+//         }
+//         resultHash.push(str);
+//     }
+
+//////////////////////
+//////////////////////
+//решение через свойства объекта
+//         var obj = {};
+//         // for (var i = 0; i < lowers.length; i++) {
+//         for (var i in lowers) {
+//             var str = lowers[i];
+//             obj[str] = true; // запомнить строку в виде свойства объекта
+//         }
+//         // var resultHash = Object.keys(obj);
+//         return Object.keys(obj);
+//////////////////////
+    //var resultHash = lowers.filter(function(item, i, ar){ return ar.indexOf(item) === i; });
+// return lowers;
+// return resultHash.join(', ').split(',').toString();
+// };
